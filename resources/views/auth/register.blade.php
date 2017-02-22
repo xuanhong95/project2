7dc6e2e785d52d8f08ef413150799633d0fa108b
xuanhong95 <mrh95hn@gmail.com>
2017-02-22 10:43:43 +0700

    fix footer alway at bottom

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.app')
-
+<link rel="stylesheet" href="/css/homepage.css">
+<style type="text/css">
+    .footer{
+        bottom: 0;
+    }
+    html{
+        overflow: hidden;
+    }
+</style>
 @section('content')
-<div class="container">
+<div class="container" style="margin-top: 70px">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
