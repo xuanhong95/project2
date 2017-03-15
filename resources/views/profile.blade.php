383a04a56524ad25b60d73eaf542394743442b4f
xuanhong95 <mrh95hn@gmail.com>
2017-03-15 10:34:17 +0700

    fix dropdown menu in some page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -14,9 +14,10 @@
         </div>
     </div>
 </div>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
 <script type="text/javascript">
     $('#dob').datepicker({
         changeMonth:true,
