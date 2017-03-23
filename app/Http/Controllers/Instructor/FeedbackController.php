a5ad29645a4e71fdf29ce51fed4e940c770cbf25
lucinick <nyn548@gmail.com>
2017-03-24 00:27:59 +0700

    create view without ->save

--- app/Http/Controllers/Instructor/FeedbackController.php
+++ app/Http/Controllers/Instructor/FeedbackController.php
@@ -12,6 +12,8 @@ class FeedbackController extends Controller
     //
     public function feedbackStudent($student_id)
     {
+        $student_name = \App\User::where('id',$student_id)->select('name')->first();
+
         $technical_criterias = \App\TechnicalLevelCriteria::get();
         $task_criterias = \App\TaskCriteria::get();
         $attitude_criterias = \App\AttitudeCriteria::get();
@@ -23,12 +25,13 @@ class FeedbackController extends Controller
         }
 
         $allocation = \App\Allocation::where([
-            ['season','=',$openningSeason],
+            ['season','=',$openningSeason->id],
             ['student_id','=',$student_id]
-        ])->first();
+            ])->first();
 
         $company_appreciations = \App\CompanyAppreciation::where('id_allocation',$allocation->id)
-            ->first();
+        ->first();
+
 
         if(is_null($company_appreciations)){
             $company_appreciations = new \App\CompanyAppreciation;
@@ -37,20 +40,56 @@ class FeedbackController extends Controller
             $company_appreciations->save();
 
             $company_appreciations = \App\CompanyAppreciation::where('id_allocation',$allocation->id)
-                ->first();
+            ->first();
         }
 
-        $student_technical = \App\TechnicalLevel::where('id_allocation',$allocation->id)
-            ->first();
-        $student_task = \App\Task::::where('id_allocation',$allocation->id)
-            ->first();
-        $student_attitude = \App\Attitude::where('id_allocation',$allocation->id)
-            ->first();
-        $student_improvement = \App\Improvement::where('id_allocation',$allocation->id)
-            ->first();
+            $student_technicals = \App\TechnicalLevel::where('id_allocation',$allocation->id)
+            ->get();
+            $student_tasks = \App\Task::where('id_allocation',$allocation->id)
+            ->get();
+            $student_attitudes = \App\Attitude::where('id_allocation',$allocation->id)
+            ->get();
+            $student_attitudes = \App\Improvement::where('id_allocation',$allocation->id)
+            ->get();
 
-        $technical_form = \DataForm::source($student_technical);
-        $technical_form->add('related_technical',$technical_criterias[0],'radiogroup')
-            ->option(1,'Xuất sắc')->option(1,'A')->option(1,'A')->option(1,'A')->option(1,'A')->option(1,'A')
+            if(is_null($student_technicals) && is_null($student_tasks)
+                && is_null($student_attitudes) && is_null($student_improvements)){
+
+
+            }
+
+            $form = \DataForm::create();
+
+            $form->add('student_name','Name','text')->insertValue($student_name->name)
+                ->mode('readonly');
+
+            //Technical table
+            $form->add('point',$technical_criterias[0]->criteria,'radiogroup')
+                ->option('A','Xuất sắc')->option('B','Tốt')->option('C','Khá')
+                ->option('D','Bình thường')->option('F','Kém')->option('X','Không ý kiến')
+                ;
+            $form->add('comment','Nhận xét','redactor');
+
+            $form->add('general_appreciation_id',"Đánh giá chung",'select')
+                ->option('A','A')->option('B','B')->option('C','C')
+                ->option('D','D')->option('E','E')->option('F','F');
+            $form->add('is_continue_receive',"nhận tiếp",'checkbox');
+            $form->add('missing_knownledge',"Kiến thức thiếu",'checkboxgroup')
+                ->option('1','Kiến thức cơ sở (giải thuật, toán, …) ?')
+                ->option('2','Ngôn ngữ lập trình ?')
+                ->option('3','Phần mềm ?')
+                ->option('4','Phần cứng ?')
+                ->option('5','Khác ');
+            $form->add('necessary_knownledge',"kiến thức cần",'redactor');
+            $form->add('is_language_necessary',"Ngoại ngữ thiết yếu k?",'checkbox');
+            $form->add('is_language_met',"đáp ứng k?",'checkbox');
+            $form->add('shortcoming',"thiếu sót",'redactor');
+            $form->add('advantage','ưu điểm','redactor');
+            $form->add('procedure_improvement','cải tiến','redactor');
+
+            $form->submit('Send');
+            $form->build();
+
+            return view('instructor.feedback',compact('form','student_name'));
+        }
     }
-}
---
+++ resources/views/instructor/feedback.blade.php
@@ -0,0 +1,293 @@
+@extends('layouts.app');
+@section('content')
+<style media="screen">
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+    text-indent:10px;
+}
+textarea{
+    max-height: 50px;
+}
+</style>
+    <div class="container" style="margin-top:70px">
+        <div class="col-md-10 col-md-offset-1 well">
+            <legend>Feedback:</legend>
+            <div class="col-md-10 col-md-offset-1 row">
+                {!! $form->header !!}
+            </div>
+            <div class="col-md-10">
+                <h4 class="text-success">{!! $form->message !!}</h4>
+            </div>
+            <div class="col-md-10 col-md-offset-1 row">
+                <label for="student_name" class="col-md-2">Sinh viên:</label>
+                <div class="col-md-6">
+                    <strong>{!! $form->field('student_name') !!}</strong>
+                </div>
+            </div>
+            <br>
+            <br>
+            <br>
+            <div class="row col-md-10 col-md-offset-1">
+                <h4>A- Nhận xét về sinh viên thực tập</h4>
+                <div class="row">
+                    <legend><h5>1.Trình độ kỹ thuật</h5></legend>
+                    <table class="col-md-12" >
+                        <tr class="text-center">
+                            <td class="col-md-3">Tiêu chí</td>
+                            <td class="col-md-5">Điểm</td>
+                            <td>Nhận xét</td>
+                        </tr>
+                        <tr>
+                            <td>Trình độ kỹ thuật liên quan đến đề tài thực tập</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Khả năng nắm bắt các kỹ thuật mới</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Mức độ làm chủ kỹ thuật, công nghệ sau khi đã được hướng dẫn</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Khả năng phân tích</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Phương pháp luận – cách thức tổ chức công việc</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Óc sáng tạo</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Trình độ ngoại ngữ phục vụ cho công việc</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+                        </tr>
+                    </table>
+                </div>
+                <br>
+                <br>
+                <div class="row">
+                    <legend><h5>2.Công việc đã thực hiện</h5></legend>
+                    <table class="col-md-12" >
+                        <tr class="text-center">
+                            <td>Tiêu chí</td>
+                            <td>Điểm</td>
+                            <td>Nhận xét</td>
+                        </tr>
+                        <tr>
+                            <td>Khối lượng công việc đã thực hiện trong thời gian thực tập</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Chất lượng công việc đã thực hiện trong thời gian thực tập</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Khả năng tự hoàn thành công việc và cách giải quyết các vấn đề phát sinh</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Viết tài liệu về công việc đã thực hiện</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Thuyết trình</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Tuân thủ các ràng buộc chất lượng công việc của cơ sở thực tập</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+
+                    </table>
+                </div>
+                <br>
+                <br>
+                <div class="row">
+                    <legend><h5>3.Thái độ, ý thức</h5></legend>
+                    <table class="col-md-12" >
+                        <tr class="text-center">
+                            <td>Tiêu chí</td>
+                            <td>Điểm</td>
+                            <td>Nhận xét</td>
+                        </tr>
+                        <tr>
+                            <td>Đúng giờ</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Vẻ ngoài (quần áo, tác phong nơi công sở, …)</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Giữ gìn hình ảnh cho cơ sở thực tập và cho sản phẩm đã thực hiện trong quá trình làm việc</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Làm việc nhóm</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Quan hệ, giao tiếp với nhân viên, khách hàng của cơ sở thực tập</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Tuân thủ các quy định làm việc của công ty và cam kết khi thực tập</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+                        </tr>
+                    </table>
+                </div>
+                <br>
+                <br>
+                <div class="row">
+                    <legend><h5>4.Tiến bộ trong quá trình thực tập</h5></legend>
+                    <table class="col-md-12" >
+                        <tr class="text-center">
+                            <td>Tiêu chí</td>
+                            <td>Điểm</td>
+                            <td>Nhận xét</td>
+                        </tr>
+                        <tr>
+                            <td>Cải thiện năng lực, trình độ kỹ thuật</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Cải thiện thái độ, ý thức</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+
+                        </tr>
+                        <tr>
+                            <td>Cải thiện về phương pháp làm việc</td>
+                            <td>{!! $form->field('point') !!}</td>
+                            <td>{!! $form->field('comment') !!}</td>
+                        </tr>
+                    </table>
+                </div>
+                <br>
+                <br>
+                <div class="row">
+                    <legend><h5>5.Đánh giá chung về khóa thực tập</h5></legend>
+                    <table>
+                        <tr>
+                            <td class="col-md-10">Kết quả chung :<br>
+                                A : sinh viên chủ động hoàn thành các công việc, kết quả xuất sắc <br>
+                                B : sinh viên đáp ứng đầy đủ các yêu cầu công việc, kết quả tốt <br>
+                                C : sinh viên có khả năng trung bình, kết quả đạt yêu cầu <br>
+                                D : sinh viên chưa đạt hết các mục tiêu đặt ra, nhưng có cố gắng, nỗ lực, <br>
+                                F : ý thức học tập của sinh viên kém, không đạt yêu cầu <br>
+                            </td>
+                            <td>{!! $form->field('general_appreciation_id') !!}</td>
+                        </tr>
+                        <tr>
+                            <td>Quý vị có muốn tiếp tục nhận sinh viên thực tập đợt sau không ?</td>
+                            <td>{!! $form->field('is_continue_receive') !!}</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <br>
+            <br>
+            <br>
+            <div class="col-md-10 col-md-offset-1">
+                <h4>B- Đóng góp cho chương trình đào tạo (những ý kiến trong mục
+                    này của quý vị sẽ giúp chúng tôi cải tiến chương trình đào tạo,
+                    và sẽ không sử dụng để đánh giá kết quả thực tập của sinh viên)
+                </h4>
+                <div class="row">
+                    <div class="col-md-12 form-group">
+                        <label for="missing_knownledge" class="col-md-16">1.Trong 2 tuần đầu của kỳ thực tập, sinh viên
+                            chưa nắm vững các nhóm kiến thức nào?</label>
+                        <div class="col-md-11 col-md-offset-1">
+                            {!! $form->field('missing_knownledge') !!}
+                        </div>
+                    </div>
+                    <div class="col-md-12 form-group">
+                        <label for="necessary_knownledge">2.Theo quý vị, nhà trường cần chú trọng đào tạo thêm nhóm
+                            kiến thức nào cho sinh viên ?</label>
+                        <div class="col-md-11 col-md-offset-1">
+                            {!! $form->field('necessary_knownledge') !!}
+                        </div>
+                    </div>
+                    <div class="col-md-12 form-group">
+                        <label for="is_language_necessary">3.Ngoại ngữ phải là yêu cầu thiết yếu trong công
+                            việc tại cơ sở thực tập hay không ?</label>
+                        <div class="col-md-11 col-md-offset-1">
+                            {!! $form->field('is_language_necessary') !!}
+                        </div>
+                    </div>
+                    <div class="col-md-12 form-group">
+                        <label for="is_language_met">4.Nếu có, trình độ tiếng Anh / Pháp của sinh viên c
+                            ó đáp ứng yêu cầu không ?</label>
+                        <div class="col-md-11 col-md-offset-1">
+                            {!! $form->field('is_language_met') !!}
+                        </div>
+                    </div>
+                    <div class="col-md-12 form-group">
+                        <label for="shortcoming">5.Vui lòng chỉ ra một số thiếu sót của sinh viên trong quá trình thực tập ?</label>
+                        <div class="col-md-11 col-md-offset-1">
+                            {!! $form->field('shortcoming') !!}
+                        </div>
+                    </div>
+                    <div class="col-md-12 form-group">
+                        <label for="advantage">6.Vui lòng chỉ ra các ưu điểm của sinh viên trong quá trình thực tập ?</label>
+                        <div class="col-md-11 col-md-offset-1">
+                            {!! $form->field('advantage') !!}
+                        </div>
+                    </div>
+                    <div class="col-md-12 form-group">
+                        <label for="procedure_improvement">7.Theo quý vị, có cần cải tiến quy trình thực tập không ?
+                            Cải tiến như thế nào ?</label>
+                        <div class="col-md-11 col-md-offset-1">
+                            {!! $form->field('procedure_improvement') !!}
+                        </div>
+                    </div>
+                </div>
+                {!! $form->footer !!}
+            </div>
+
+        </div>
+    </div>
+@endsection
