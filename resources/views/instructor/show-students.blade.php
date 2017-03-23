7e8e237e8362056a1334c7ff9ab942da9231df0c
lucinick <nyn548@gmail.com>
2017-03-23 14:45:42 +0700

    fix name in route instructor-student

--- resources/views/instructor/show-students.blade.php
+++ resources/views/instructor/show-students.blade.php
@@ -1,37 +1,36 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container" style="margin-top:70px">
-        <div class="col-md-10 col-md-offset-1 well">
-            <legend>Students</legend>
-            <div class="col-md-12 btn btn-default">
-                @foreach( $students as $student)
-                    <div class="col-md-12">
-                        <div class="col-md-6 row">
-                            <div class="col-md-11 col-md-offset-1 row">
-                                <h4 class="text-left">Name: <a href="#" class="text-center">{!! $student->name !!}</a></h4>
-                            </div>
-                            <div class="col-md-12 text-center row">
-                                <div class="col-md-6">
-                                    <p>Class: {!! $student->class !!}</p>
-                                </div>
-                                <div class="col-md-6">
-                                    <p>Student number: {!! $student->student_number !!}</p>
-                                </div>
-                            </div>
+<div class="container" style="margin-top:70px">
+    <div class="col-md-10 col-md-offset-1 well">
+        <legend>Students</legend>
+        <div class="col-md-12 btn btn-default">
+            @foreach( $students as $student)
+            <div class="col-md-12">
+                <div class="col-md-6 row">
+                    <div class="row">
+                        <h4 class="text-center"><a href="#"><strong>{!! $student->name !!}</strong></a></h4>
+                    </div>
+                    <div class="text-center row">
+                        <div class="col-md-6">
+                            <p>Class: {!! $student->class !!}</p>
                         </div>
                         <div class="col-md-6">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <a href="{!! route('commit-work',['student_id'=>$student->user_id]) !!}">Works</a>
-                                </div>
-                                <div class="col-md-6">
-
-                                </div>
-                            </div>
+                            <p>Student number: {!! $student->student_number !!}</p>
                         </div>
                     </div>
-                @endforeach
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="{!! route('commit-work',['student_id'=>$student->user_id]) !!}">Works</a>
+                        </div>
+                        <div class="col-md-6">
+                        </div>
+                    </div>
+                </div>
             </div>
+            @endforeach
         </div>
     </div>
+</div>
 @endsection
